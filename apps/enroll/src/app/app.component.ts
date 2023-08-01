de80f227057acb5982e14ebf1008db35aadedd9d
Brad <bbodine1@gmail.com>
2023-08-01 10:11:32 -0500

    copying enroll to new repo

--- apps/enroll/src/app/app.component.ts
+++ apps/enroll/src/app/app.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { HeaderComponent } from '@enroll/header';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [HeaderComponent, RouterModule],
   selector: 'enroll-root',
   template: `
     <h1>Welcome {{ title }}</h1>
 
+    <!-- <enroll-header phoneNumber="800-965-7476" tty="711"></enroll-header> -->
+    <enroll-header phoneNumber="800-965-7476" tty="711"></enroll-header>
+
     <div class="content-container">
       <router-outlet></router-outlet>
     </div>
+
+    <!-- <enroll-footer
+      email="smallbusiness@coverme.gov"
+      phoneNumber="800-965-7476"
+      tty="711"
+      helpLink="https://www.coverme.gov/help/"
+    ></enroll-footer> -->
   `,
 })
 export class AppComponent {
