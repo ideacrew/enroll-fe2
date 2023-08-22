14fd2462a25c55cb3292ac2866e394ec68a525e4
Brad Bodine <bbodine1@gmail.com>
2023-08-21 22:19:22 -0500

    fix animation console error

--- apps/sbtcc/src/app/app.config.ts
+++ apps/sbtcc/src/app/app.config.ts
@@ -4,7 +4,7 @@ import {
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
 import { appRoutes } from './app.routes';
-// import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideStore } from '@ngrx/store';
 // import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
@@ -19,6 +19,6 @@ export const appConfig: ApplicationConfig = {
     provideStoreDevtools({ logOnly: !isDevMode() }),
     // provideState(fromData.DATA_FEATURE_KEY, fromData.dataReducer),
     // provideEffects(DataEffects),
-    // provideAnimations(),
+    provideAnimations(),
   ],
 };
